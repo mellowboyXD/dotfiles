b1d1f43e7be7868127367964e0abee35265ae432
mellowXD <chamerallyishfaq@gmail.com>
2024-02-24 19:01:52 +0400

    Tweak

--- nvim/.config/nvim/after/plugin/colors.lua
+++ nvim/.config/nvim/after/plugin/colors.lua
@@ -61,5 +61,5 @@ require("rose-pine").setup({
 
 -- vim.cmd("colorscheme rose-pine")
 -- vim.cmd("colorscheme rose-pine-main")
-vim.cmd("colorscheme rose-pine-moon")
+vim.cmd("colorscheme rose-pine-dawn")
 -- vim.cmd("colorscheme rose-pine-dawn")
