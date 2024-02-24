61b36a0b9d80dfbef912d81505f8d050a42bbf83
mellowXD <chamerallyishfaq@gmail.com>
2024-02-24 19:05:38 +0400

    Tweak

--- nvim/.config/nvim/after/plugin/colors.lua
+++ nvim/.config/nvim/after/plugin/colors.lua
@@ -6,13 +6,13 @@ vim.cmd [[set scrolloff=8]]
 
 require("rose-pine").setup({
     variant = "auto", -- auto, main, moon, or dawn
-    dark_variant = "main", -- main, moon, or dawn
+    dark_variant = "moon", -- main, moon, or dawn
     dim_inactive_windows = false,
     extend_background_behind_borders = true,
 
     enable = {
         terminal = true,
-        legacy_highlights = true, -- Improve compatibility for previous versions of Neovim
+        legacy_highlights = false, -- Improve compatibility for previous versions of Neovim
         migrations = true, -- Handle deprecated options automatically
     },
 
@@ -61,5 +61,5 @@ require("rose-pine").setup({
 
 -- vim.cmd("colorscheme rose-pine")
 -- vim.cmd("colorscheme rose-pine-main")
-vim.cmd("colorscheme rose-pine-dawn")
+vim.cmd("colorscheme rose-pine-moon")
 -- vim.cmd("colorscheme rose-pine-dawn")
