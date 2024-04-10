61aa777eed2a6d8e4c3b703dadf61e369931ba77
mellowXD <chamerallyishfaq@gmail.com>
2024-04-10 22:36:51 +0400

    Switch from tabsize 4 to tabsize 2

--- nvim/.config/nvim/lua/mellowboy/set.lua
+++ nvim/.config/nvim/lua/mellowboy/set.lua
@@ -2,9 +2,10 @@
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
+local size = 2
+vim.opt.tabstop = size
+vim.opt.softtabstop = size
+vim.opt.shiftwidth = size
 vim.opt.expandtab = true
 
 vim.opt.smartindent = true
